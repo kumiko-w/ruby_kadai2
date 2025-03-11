a1a901d10923fa7e0f6c352cf0308c1862ec4b64
kumiko watanabe <kumiko@MacBookAir.local>
2025-03-12 01:59:16 +0900

    メモ課題提出

---
+++ memo.rb
@@ -0,0 +1,35 @@
+require 'csv'
+
+loop do
+  puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
+  memo_type = gets.to_i
+    
+  if memo_type == 1
+    puts"拡張子を除いたファイル名を入力してください(例：テスト.csvを作りたい場合はテストと入力）"
+    file_name = gets.chomp
+    puts "新しいメモの内容を入力してください"
+    puts "完了したらエンターキーを押してください"
+    memo_content = gets.chomp
+    CSV.open("#{file_name}.csv", "w") do |csv|
+    csv << [memo_content]
+  end
+    puts "#{file_name}を作成しました。"
+break # ループを終了
+        
+  elsif memo_type == 2
+    puts "編集するファイル名を拡張子を除いて入力してください（例：テスト.csvの場合はテストと入力）"
+    file_name = gets.chomp
+      puts "追記する内容を入力してください"
+      puts "完了したらエンターキーを押してください"
+    memo_content = gets.chomp
+      CSV.open("#{file_name}.csv", "a") do |csv|
+      csv << [memo_content]
+    end
+      puts "#{file_name}に追記しました"
+break # ループを終了
+        
+  else
+    puts "1か2を入力してください"
+  end
+    
+end
