55041662639bd5922dc86c56ae224c0849869f5b
kumiko watanabe <kumiko@MacBookAir.local>
2025-03-13 18:51:23 +0900

    修正

--- memo.rb
+++ memo.rb
@@ -5,31 +5,29 @@ loop do
   memo_type = gets.to_i
     
   if memo_type == 1
-    puts"拡張子を除いたファイル名を入力してください(例：テスト.csvを作りたい場合はテストと入力）"
+    puts "拡張子を除いたファイル名を入力してください(例：テスト.csvを作りたい場合はテストと入力）"
     file_name = gets.chomp
-    puts "新しいメモの内容を入力してください"
-    puts "完了したらエンターキーを押してください"
-    memo_content = gets.chomp
+    puts "新しいメモの内容を入力してください（Ctrl+D で保存）"
+    memo_content = $stdin.read
     CSV.open("#{file_name}.csv", "w") do |csv|
-    csv << [memo_content]
-  end
-    puts "#{file_name}を作成しました。"
-break # ループを終了
+      memo_content.split("\n").each { |line| csv << [line] }
+    end
+    puts "#{file_name} を作成しました。"
+    break # ループを終了
         
   elsif memo_type == 2
-    puts "編集するファイル名を拡張子を除いて入力してください（例：テスト.csvの場合はテストと入力）"
+    puts "編集するファイル名を拡張子を除いて入力してください（例：テスト.csv の場合はテストと入力）"
     file_name = gets.chomp
-      puts "追記する内容を入力してください"
-      puts "完了したらエンターキーを押してください"
-    memo_content = gets.chomp
-      CSV.open("#{file_name}.csv", "a") do |csv|
-      csv << [memo_content]
+    puts "追記する内容を入力してください（Ctrl+D で保存）"
+    memo_content = $stdin.read
+    CSV.open("#{file_name}.csv", "a") do |csv|
+      memo_content.split("\n").each { |line| csv << [line] }
     end
-      puts "#{file_name}に追記しました"
-break # ループを終了
+    puts "#{file_name} に追記しました。"
+    break # ループを終了
         
   else
     puts "1か2を入力してください"
   end
-    
 end
+
